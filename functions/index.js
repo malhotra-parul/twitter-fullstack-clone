3b07d90037b48d64e3e05da95899d010d84af579
malhotra-parul <parulm.business@gmail.com>
2020-06-14 19:20:43 +0530

    Created a login route

--- functions/index.js
+++ functions/index.js
@@ -134,6 +134,34 @@ app.post("/signup", (req, res) => {
     });
 });
 
+//Login route
+app.post("/login", (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+    if(isEmpty(user.email)) errors.email = "Must not be empty";
+    if(isEmpty(user.password)) errors.password = "Must not be empty";
+    if(Object.keys(errors).length > 0 ) {
+        return res.status(400).json(errors);
+    }
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+            .then(data => {
+                return data.user.getIdToken();
+            }).then(token => {
+                return res.json({token});
+            }).catch((err) => {
+                console.error(err);
+                if(err.code === "auth/wrong-password"){
+                    return res.status(403).json({ general: "Unauthorized access!"})
+                }else{
+                return res.status(500).json({error: err.code});
+                }
+            })
+})
+
 
 
 exports.api = functions.region("asia-east2").https.onRequest(app);
